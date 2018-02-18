fc0e76a6672a74e5c44b1aa037e43403be201400
Fabio Brito <luan.tecno@gmail.com>
2018-02-18 12:55:49 -0300

    Add: metodos de lista

--- index.php
+++ index.php
@@ -3,8 +3,23 @@
 require_once("config.php");
 
 
-$root = new Usuario();
-$root->loadByID(3);
-echo $root;
+
+//Carrega um usuario
+//$root = new Usuario();
+//$root->loadByID(3);
+//echo $root;
+
+//Carrega uma lista de usuarios;
+//$lista = Usuario::allUsers();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login;
+//$busca = Usuario::searchUser("o");
+//echo json_encode($busca);
+
+//Carrega um usuario usando um login e a senha;
+$usuario = new Usuario();
+$usuario->login("user", "123456");
+echo $usuario;
 
 ?>
