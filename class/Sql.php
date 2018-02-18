7c654e758e49747e168ad46a83be505f07b113a4
Fabio Brito <luan.tecno@gmail.com>
2018-02-18 12:21:52 -0300

    Add class Usuario

--- class/Sql.php
+++ class/Sql.php
@@ -5,17 +5,17 @@ class Sql extends PDO {
 
     public function __construct()
     {
-       $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+        $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
     }
 
-    private function setParams($statment, $parameters = array()) {
+    private function setParams($statement, $parameters = array()) {
         foreach ($parameters as $key => $value) {
-            $this->setParam($key, $value);
+            $this->setParam($statement, $key, $value);
         }
     }
 
-    private function setParam($statment, $key, $value) {
-        $statment->bindParam($key, $value);
+    private function setParam($statement, $key, $value) {
+        $statement->bindParam($key, $value);
     }
 
     public function query($rawQuery, $params = array()) {
@@ -30,8 +30,5 @@ class Sql extends PDO {
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
-
-
-
 
 ?>
